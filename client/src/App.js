de783167e82007dbea435fa575c8dea98eaab942
Ely Alamillo <ely0013@gmail.com>
2018-03-01 17:43:57 -0600

    wired up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import Register from './components/Register/Register';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="container">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/start" exact component={Register} />
+        </div>
       </div>
     );
   }
