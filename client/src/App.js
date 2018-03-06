f9d67dc6147e61d23db8db63c7c45e9040409ac0
Ely Alamillo <ely0013@gmail.com>
2018-03-05 19:02:05 -0600

    added dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import Register from './components/Register/Register';
+import Dashboard from './components/Dashboard/Dashboard';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <div className="container">
           <Route path="/" exact component={HomePage} />
           <Route path="/start" exact component={Register} />
+          <Route path="/dashboard/:uid" exact component={Dashboard} />
         </div>
       </div>
     );
