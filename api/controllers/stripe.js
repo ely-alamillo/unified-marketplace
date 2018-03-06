8ee74d24fa5c9fbeea6fd14fa90684e0ae0796bb
Ely Alamillo <ely0013@gmail.com>
2018-03-05 19:01:49 -0600

    fixed getUID

--- api/controllers/stripe.js
+++ api/controllers/stripe.js
@@ -7,7 +7,6 @@ const db = firebase.database();
 
 const createAccount = (req, res) => {
   const user = req.body;
-  console.log('user:', req.body);
   // Generate a random string as state to protect from CSRF
   const state = Math.random()
     .toString(36)
@@ -54,30 +53,63 @@ const connectAccount = (req, res) => {
   const client_id = 'ca_CPnVNqJ6MzKkU0f8rqkxGz44HjAIBxdx';
   const client_secret = 'sk_test_QSuOZ8Dn5wvjxbtPtyxMTsCS';
   const data = { code, client_id, client_secret, grant_type };
-  const uid = getUID();
-  if (uid === 'ERORR') {
-    res.json({ error: 'could not get uid' });
-  } else {
-    axios
-      .post('https://connect.stripe.com/oauth/token', data)
-      .then(data => {
-        const stripe_user_id = data.data.stripe_user_id;
-        setUserStripeId(uid, stripe_user_id);
-        res.redirect('http://localhost:3000/#/yeetboy');
-      })
-      .catch(err => {
-        res.json({ err: err, from: 'error in connecting account to stripe' });
-      });
-  }
+  // const uid = await getUID();
+  // console.log('UID from connectAccount: ', uid);
+  getUID()
+    .then(user => {
+      axios
+        .post('https://connect.stripe.com/oauth/token', data)
+        .then(data => {
+          const stripe_user_id = data.data.stripe_user_id;
+          setUserStripeId(user.uid, stripe_user_id);
+          res.redirect(`http://localhost:3000/#/dashboard/${user.uid}`);
+        })
+        .catch(err => {
+          res.json({ err: err, from: 'error in connecting account to stripe' });
+        });
+    })
+    .catch(err => {
+      console.log(error);
+      res.json({ err: err, from: 'error in connecting account to stripe' });
+    });
+
+  // if (uid === 'ERORR') {
+  //   res.json({ error: 'could not get uid' });
+  // } else {
+  //   axios
+  //     .post('https://connect.stripe.com/oauth/token', data)
+  //     .then(data => {
+  //       const stripe_user_id = data.data.stripe_user_id;
+  //       setUserStripeId(uid, stripe_user_id);
+  //       res.redirect('http://localhost:3000/#/yeetboy');
+  //     })
+  //     .catch(err => {
+  //       res.json({ err: err, from: 'error in connecting account to stripe' });
+  //     });
+  // }
 };
 
 const getUID = () => {
+  return new Promise((resolve, reject) => {
+    db
+      .ref('currentUID/secret')
+      .once('value')
+      .then(snapshot => {
+        // uid = snapshot.val().uid;
+        resolve(snapshot.val());
+      })
+      .catch(err => {
+        reject({ msg: 'failed to get uid' });
+      });
+  });
+
+  // })
   let uid = '';
   db
     .ref('currentUID/secret')
     .once('value')
     .then(snapshot => {
-      uid = snapshot.value().uid;
+      uid = snapshot.val().uid;
     })
     .catch(err => {
       uid = 'ERORR';
@@ -86,10 +118,29 @@ const getUID = () => {
 };
 
 const setUserStripeId = (uid, stripe_user_id) => {
+  console.log('UID: ', uid);
   return db
     .ref()
     .child('users/' + uid)
     .update({ stripe_user_id });
 };
 
-module.exports = { createAccount, connectAccount };
+const test = (req, res) => {
+  let uid = '';
+  db
+    .ref('currentUID/secret')
+    .once('value')
+    .then(snapshot => {
+      uid = snapshot.val().uid;
+      console.log('uid inside getUID: ', uid);
+      console.log('snapshot.value().uid: ', snapshot.val().uid);
+      res.json({ success: true });
+    })
+    .catch(error => {
+      console.log('error:', error);
+      res.json({ err: true });
+    });
+  // return uid;
+};
+
+module.exports = { createAccount, connectAccount, test };
