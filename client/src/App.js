642db2fd7cb415dc87d409bce94af9686767abf4
Ely Alamillo <ely0013@gmail.com>
2018-03-07 12:42:09 -0600

    wired up donate route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import Register from './components/Register/Register';
 import Dashboard from './components/Dashboard/Dashboard';
+import Donate from './components/Donate/Donate';
 import './App.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="container">
           <Route path="/" exact component={HomePage} />
           <Route path="/start" exact component={Register} />
+          <Route path="/donate" exact component={Donate} />
           <Route path="/dashboard/:uid" exact component={Dashboard} />
         </div>
       </div>
